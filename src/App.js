eeed4167cb589e03fb630b6acc86d180a3c5f8fe
Ramesh Kunwar <rameshkunwar@hotmail.com>
2020-02-20 22:37:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import allActions from "./redux/actions";
+import logo from "./logo.svg";
+import "./App.css";
+import { CheckboxOperation } from "./Checkboxes";
+
+const App = () => {
+  const counter = useSelector((state) => state.counter);
+  const currentUser = useSelector((state) => state.currentUser);
+
+  const dispatch = useDispatch();
+
+  const user = { id: 123, name: "t-rox" };
+
+  useEffect(() => {
+    dispatch(allActions.userActions.setUser(user));
+    dispatch(allActions.counterActions.increment());
+    dispatch(allActions.counterActions.decrement());
+  }, []);
+
+  return (
+    <div className='App'>
+      <div className='workspace-container'>
+        {currentUser.loggedIn ? (
+          <React.Fragment>
+            <h1>Welcome {currentUser.user.name}</h1>
+            <button onClick={() => dispatch(allActions.userActions.logOut())}>
+              Logout
+            </button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <h1>Login</h1>
+            <button
+              onClick={() => dispatch(allActions.userActions.setUser(user))}
+            >
+              Log in a t-rex
+            </button>
+          </React.Fragment>
+        )}
+
+        <h1>Counter = {counter}</h1>
+
+        <button onClick={() => dispatch(allActions.counterActions.increment())}>
+          Increase
+        </button>
+        <button onClick={() => dispatch(allActions.counterActions.decrement())}>
+          Decrease
+        </button>
+
+        {/* <h1>My workspace</h1>
+        <div className='checkboxes-operation'>
+          <CheckboxOperation />
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/redux/actions/index.js
@@ -0,0 +1,9 @@
+import counterActions from "./counterAction";
+import userActions from "./userActions";
+
+const allActions = {
+  counterActions,
+  userActions
+};
+
+export default allActions;
